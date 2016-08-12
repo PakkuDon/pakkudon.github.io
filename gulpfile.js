5b197fac9bcb0c794a09d18a1b446eafa0ec341b
PakkuDon <pakku.pd@gmail.com>
2016-08-12 23:19:43 +1000

    Edited gulpfile
    - Added watch task - recompiles script files and stylesheets on change
    - Added Babel and Sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
 
 // Minify library files
 gulp.task('vendor', function() {
@@ -18,12 +20,28 @@ gulp.task('vendor', function() {
         .pipe(gulp.dest('./dist/js'));
 });
 
+// Minify source files
+gulp.task('bundle', function() {
+    return gulp.src('app/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(concat('bundle.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
 // Compile Sass stylesheets
 gulp.task('sass', function() {
     return gulp.src('./app/stylesheets/*.scss')
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(gulp.dest('./dist/css'));
+});
+
+// Recompile project files on change
+gulp.task('watch', function() {
+    gulp.watch('app/stylesheets/*.scss', ['sass']);
+    gulp.watch('app/**/*.js', ['bundle']);
 });
 
 // Compile third-party stylesheets
@@ -35,4 +53,5 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('./dist/css'));
 })
 
-gulp.task('default', ['vendor', 'sass', 'styles']);
+gulp.task('default', ['vendor', 'watch', 'styles']);
+gulp.task('build', ['vendor', 'bundle', 'sass', 'styles']);
