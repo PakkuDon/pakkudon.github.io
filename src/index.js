df4e9a954f78c96865c2b8d4c4a036a1a14f5832
PakkuDon <pakku.pd@gmail.com>
2016-12-21 16:01:04 +1100

    Add project stubs back to page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 require('normalize.css/normalize.css');
 require('./stylesheets/style.scss');
-var projectTemplate = require('./templates/project-stub.hbs');
+import fetch from 'isomorphic-fetch';
+import projectTemplate from './templates/project-stub.hbs';
+
+var projectListElem = document.querySelector('.project-list');
+
+fetch('/src/data/projects.json')
+  .then(res => res.json())
+  .then(projects => {
+    var projectStubs = projects.map(project => projectTemplate({ project }));
+    projectListElem.insertAdjacentHTML('beforeend', projectStubs.join(''));
+  });
 
 console.log(`Hello this is ${'dog'}`);
