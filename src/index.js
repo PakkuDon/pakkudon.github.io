a5aa65db395566cf2d4885875fea297f636e3cc2
PakkuDon <pakku.pd@gmail.com>
2016-12-21 17:32:00 +1100

    Add skill lists to about section

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ require('normalize.css/normalize.css');
 require('./stylesheets/style.scss');
 import fetch from 'isomorphic-fetch';
 import projectTemplate from './templates/project-stub.hbs';
+import tagListTemplate from './templates/tag-list.hbs';
 
-var projectListElem = document.querySelector('.project-list');
-
+// Load project data
 fetch('/src/data/projects.json')
   .then(res => res.json())
   .then(projects => {
+    var projectListElem = document.querySelector('.project-list');
     var projectStubs = projects.map(project => projectTemplate({ project }));
     projectListElem.insertAdjacentHTML('beforeend', projectStubs.join(''));
   });
 
-console.log(`Hello this is ${'dog'}`);
+// Load skill data
+fetch('/src/data/skills.json')
+  .then(res => res.json())
+  .then(skills => {
+    var skillListsElem = document.querySelector('.skill-lists');
+    var keys = Object.keys(skills);
+    var tagLists = keys.map(key => tagListTemplate({
+      title: key,
+      tags: skills[key]
+    }));
+    skillListsElem.insertAdjacentHTML('beforeend', tagLists.join(''));
+  });
