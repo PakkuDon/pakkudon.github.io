6df3f9c6645dfebc767edcac06316f7e878dcfe5
PakkuDon <pakku.pd@gmail.com>
2021-02-28 20:09:38 +1100

    Include project and skill data at compile-time
    These are relatively small static documents
    so they don't need to be fetched on page load

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "languages": [
     "JavaScript",
     "Ruby",
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 require('normalize.css/normalize.css');
 require('./stylesheets/style.scss');
-import fetch from 'isomorphic-fetch';
 import projectTemplate from './templates/project-stub.hbs';
 import tagListTemplate from './templates/tag-list.hbs';
 import LightBox from './lightbox';
 import smoothScroll from 'smoothscroll';
+import projects from './data/projects'
+import skills from './data/skills'
 
 // Initialize lightbox
 LightBox.initialize(document.querySelector('.lightbox'));
@@ -29,44 +30,36 @@ window.addEventListener('scroll', e => {
   }
 });
 
-// Load project data
-fetch('/src/data/projects.json')
-  .then(res => res.json())
-  .then(projects => {
-    var projectListElem = document.querySelector('.project-list');
-    var projectStubs = projects.map(project => {
-      var html = projectTemplate({ project });
-      var tempDiv = document.createElement('div');
-      tempDiv.innerHTML = html;
+// Display project data
+var projectListElem = document.querySelector('.project-list');
+var projectStubs = projects.map(project => {
+  var html = projectTemplate({ project });
+  var tempDiv = document.createElement('div');
+  tempDiv.innerHTML = html;
 
-      var showImages = images => {
-        LightBox.setImages(images);
-        LightBox.show();
-      };
+  var showImages = images => {
+    LightBox.setImages(images);
+    LightBox.show();
+  };
 
-      var thumbnail = tempDiv.querySelector('.project-thumbnail');
-      thumbnail.addEventListener('click', () => showImages(project.image_set));
-      thumbnail.addEventListener('keydown', (e) => {
-        if (e.code === 'Space') {
-          showImages(project.image_set);
-        }
-      });
-
-      return tempDiv.firstChild;
-    });
-
-    projectStubs.forEach(stub => projectListElem.appendChild(stub));
+  var thumbnail = tempDiv.querySelector('.project-thumbnail');
+  thumbnail.addEventListener('click', () => showImages(project.image_set));
+  thumbnail.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') {
+      showImages(project.image_set);
+    }
   });
 
-// Load skill data
-fetch('/src/data/skills.json')
-  .then(res => res.json())
-  .then(skills => {
-    var skillListsElem = document.querySelector('.skill-lists');
-    var keys = Object.keys(skills);
-    var tagLists = keys.map(key => tagListTemplate({
-      title: key,
-      tags: skills[key]
-    }));
-    skillListsElem.insertAdjacentHTML('beforeend', tagLists.join(''));
-  });
+  return tempDiv.firstChild;
+});
+
+projectStubs.forEach(stub => projectListElem.appendChild(stub));
+
+// Display skill data
+var skillListsElem = document.querySelector('.skill-lists');
+var keys = Object.keys(skills);
+var tagLists = keys.map(key => tagListTemplate({
+  title: key,
+  tags: skills[key]
+}));
+skillListsElem.insertAdjacentHTML('beforeend', tagLists.join(''));
